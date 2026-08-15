6b72e2a8ea63b755562ed36ffe66df64e12ef4f6
dwwob <wobker39@yahoo.com>
2026-08-14 23:53:46 -0500

    up dated conform settings

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,36 +1,94 @@
 return {
 	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" }, -- Load when opening a file
-	config = function()
-		local conform = require("conform")
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "[F]ormat buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = { "prettierd", "prettier", stop_after_first = true },
+			typescript = { "prettierd", "prettier", stop_after_first = true },
+			rust = { "rustfmt", lsp_format = "fallback" },
+			c = { "clang-format" },
+			cpp = { "clang-format" },
+			html = { "prettierd", "prettier", stop_after_first = true },
+			css = { "prettierd", "prettier", stop_after_first = true },
+			json = { "prettierd", "prettier", stop_after_first = true },
+			jsonc = { "prettierd", "prettier", stop_after_first = true },
+			bash = { "shfmt" },
+			sh = { "shfmt" },
+			zsh = { "shfmt" },
 
-		conform.setup({
-			formatters_by_ft = {
-				html = { "prettier" },
-				css = { "prettier" },
-				lua = { "stylua" },
-				python = { "isort", "black" }, -- Runs sequentially
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				json = { "prettier" },
-			},
-			-- Trigger auto-format on save
-			format_on_save = {
-				lsp_fallback = true, -- Fallback to LSP formatting if no formatter is found
-				async = false,
-				timeout_ms = 500,
-			},
-		})
+			-- Native Pascal configuration
+			pascal = { "ptop" },
+		},
 
-		-- Optional keymap to format manually
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+		-- Custom shfmt spacing overrides
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2", "-ci" },
+			},
+		},
+
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
 }
+
+-- return {
+-- 	"stevearc/conform.nvim",
+-- 	event = { "BufReadPre", "BufNewFile" }, -- Load when opening a file
+-- 	config = function()
+-- 		local conform = require("conform")
+--
+-- 		conform.setup({
+-- 			formatters_by_ft = {
+-- 				html = { "prettier" },
+-- 				css = { "prettier" },
+-- 				lua = { "stylua" },
+-- 				python = { "isort", "black" }, -- Runs sequentially
+-- 				javascript = { "prettier" },
+-- 				typescript = { "prettier" },
+-- 				javascriptreact = { "prettier" },
+-- 				typescriptreact = { "prettier" },
+-- 				json = { "prettier" },
+--
+--
+--         -- ["*"] = { "codespell" },
+--
+--  -- 2. Use the "_" key for files without explicit language tools configured above
+--       -- ["_"] = { "squeeze_blank", "trim_whitespace", "trim_newlines" },
+--     },
+-- 			},
+--
+--
+-- 			-- Trigger auto-format on save
+-- 			format_on_save = {
+-- 				lsp_fallback = true, -- Fallback to LSP formatting if no formatter is found
+-- 				async = false,
+-- 				timeout_ms = 500,
+-- 			},
+-- 		})
+--
+-- 		-- Optional keymap to format manually
+-- 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+-- 			conform.format({
+-- 				lsp_fallback = true,
+-- 				async = false,
+-- 				timeout_ms = 500,
+-- 			})
+-- 		end, { desc = "Format file or range (in visual mode)" })
+-- 	end,
+-- }
